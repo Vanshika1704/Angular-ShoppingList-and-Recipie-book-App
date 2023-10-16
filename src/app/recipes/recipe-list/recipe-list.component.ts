248fc12a1d92ad19dc08e3020dd4c2d8b45de0cb
Vanshika Wadhwa <39941736+Vanshika1704@users.noreply.github.com>
2023-10-16 08:22:49 +0000

    Update 250 files

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'this is simply a test recipe',
+      'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimg.taste.com.au%2FNltXC_f3%2Fw1200-h630-cfill%2Ftaste%2F2022%2F09%2Fall-in-one-tuscan-chicken-pasta-bake-181120-2.jpg&tbnid=SD3NpH4xsa9LBM&vet=12ahUKEwjB5fOcgvqBAxVscWwGHXe3CXwQMygTegUIARCeAQ..i&imgrefurl=https%3A%2F%2Fwww.taste.com.au%2Frecipes%2Fcollections%2Fpasta-recipes&docid=c0-fvJK0WqQl4M&w=1200&h=630&q=pasta%20image&safe=active&ved=2ahUKEwjB5fOcgvqBAxVscWwGHXe3CXwQMygTegUIARCeAQ',
+    ),
+    new Recipe(
+      'A test recipe',
+      'this is simply a test recipe',
+      'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimg.taste.com.au%2FNltXC_f3%2Fw1200-h630-cfill%2Ftaste%2F2022%2F09%2Fall-in-one-tuscan-chicken-pasta-bake-181120-2.jpg&tbnid=SD3NpH4xsa9LBM&vet=12ahUKEwjB5fOcgvqBAxVscWwGHXe3CXwQMygTegUIARCeAQ..i&imgrefurl=https%3A%2F%2Fwww.taste.com.au%2Frecipes%2Fcollections%2Fpasta-recipes&docid=c0-fvJK0WqQl4M&w=1200&h=630&q=pasta%20image&safe=active&ved=2ahUKEwjB5fOcgvqBAxVscWwGHXe3CXwQMygTegUIARCeAQ',
+    ),
+  ];
   constructor() {}
   ngOnInit() {}
 }
--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,12 +1,11 @@
-//no decorators like @Model are needed- recipe model can be created as a simple class 
-export class Recipe{
-    public name: string;
-    public description: string;
-    public imagePath:string; //url
-    constructor(name:String, description:string,imagePath:string){
-        this.name=name;
-        this.description=description;
-        this.imagePath=imagePath;
-
-    }
+//no decorators like @Model are needed- recipe model can be created as a simple class
+export class Recipe {
+  public name: string;
+  public description: string;
+  public imagePath: string; //url
+  constructor(name: string, description: string, imagePath: string) {
+    this.name = name;
+    this.description = description;
+    this.imagePath = imagePath;
+  }
 }
